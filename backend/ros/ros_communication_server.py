6e9cadeba20f9a447c04d9220602011dfdffa318
Yousef Nassar <ynassar@github.com>
2018-10-13 19:29:32 +0100

    Adds fake ROS RPC server.

---
+++ backend/ros/ros_communication_server.py
@@ -0,0 +1,31 @@
+import grpc
+import mongoengine
+import time
+
+from absl import flags
+from absl import app
+from backend.ros import ros_communication_service
+from concurrent import futures
+from proto.ros import ros_pb2_grpc
+
+flags.DEFINE_integer("port", 50051, "The port on which to run the server.")
+
+FLAGS = flags.FLAGS
+
+_ONE_DAY_IN_SECONDS = 60*60*24
+
+def main(argv):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    ros_pb2_grpc.add_RosServiceServicer_to_server(
+        ros_communication_service.RosService(), server)
+    server.add_insecure_port(f'[::]:{FLAGS.port}')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    app.run(main)
