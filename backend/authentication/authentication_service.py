195ffab19ddd9f5f3f8f79fbc4eb9cc637a14952
Yousef Nassar <yousefnassar@aucegypt.edu>
2018-10-17 16:45:33 +0200

    Raises Exceptions on invalid  parameters in addition to setting status codes to bypass a swift-GRPC limitation. (#17)

--- backend/authentication/authentication_service.py
+++ backend/authentication/authentication_service.py
@@ -8,6 +8,12 @@ from backend.models import user as user_model
 from proto.login import login_pb2
 from proto.login import login_pb2_grpc
 
+class NoSuchUser(Exception):
+    pass
+
+class WrongPassword(Exception):
+    pass
+
 class AuthenticationService(login_pb2_grpc.AuthenticationServiceServicer):
     def __init__(self, rsa_key):
         self._rsa_key = rsa_key.encode('utf-8')
@@ -18,9 +24,11 @@ class AuthenticationService(login_pb2_grpc.AuthenticationServiceServicer):
         except mongoengine.DoesNotExist:
             context.set_code(grpc.StatusCode.NOT_FOUND)
             context.set_details("No user found with the given details.")
+            raise NoSuchUser()
         if bcrypt.checkpw(request.password.encode('utf-8'), user.password_hash.encode('utf-8')):
             token = jwt.encode({'username':user.username}, self._rsa_key)
             return login_pb2.LoginResponse(token=token)
         else:
             context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
             context.set_details("Invalid password.")
+            raise WrongPassword()
