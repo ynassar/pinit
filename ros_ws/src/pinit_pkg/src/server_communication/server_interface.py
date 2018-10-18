9aa70d35e7b52a51bfe7358894f0b63dc883493f
kareefardi <kareefardi@users.noreply.github.com>
2018-10-19 01:44:52 +0300

    Feature/ros server communication node (#21)
    * added ros grpc server interface and mapping request handler + bug fixes
    
    * removed unnecessary exceptions + change some naming + misc

--- ros_ws/src/pinit_pkg/src/server_communication/server_interface.py
+++ ros_ws/src/pinit_pkg/src/server_communication/server_interface.py
@@ -1,18 +1,51 @@
-from proto.ros import ros_pb2_grpc
-from proto.ros import ros_pb2
+#!/usr/bin/env python
+
 import grpc
 
-def SendData():
-    # generate some data
-    yield ros_pb2.RosToServerCommunication()
+from proto.ros import ros_pb2_grpc
+from proto.ros import ros_pb2
+
+from enum import Enum
+
+from server_mapping_handler import ServerMappingHandler
+
+import rospy
 
 
-def main():
-    with grpc.insecure_channel('10.40.37.234:50051') as channel:
-        stub = ros_pb2_grpc.RosServiceStub(channel)
-        for communication in stub.Communicate(SendData()):
-            print(communication)
-            
+class ServerHandler():
+
+    def __init__(self):
+
+        self.robot_name = "nemo"
+        self.node_name = "robot_grpc_server_handler"
+        self.mapping_hanlder = ServerMappingHandler()
+    
+        self.init_node()
+        self.main_loop()
+
+
+    def init_node(self):
+        rospy.init_node(self.node_name, anonymous=False)
+
+
+    def stub_callback(self):
+        yield ros_pb2.RosToServerCommunication(robot_name=self.robot_name)
+
+
+    def main_loop(self):
+        while not rospy.is_shutdown():
+            with grpc.insecure_channel('10.40.54.241:50052') as channel:
+                stub = ros_pb2_grpc.RosServiceStub(channel)
+                for communication in stub.Communicate(self.stub_callback()):
+                    print communication
+                    if communication.HasField("mapping_request"):
+                        mapping_request = communication.mapping_request
+                        self.mapping_hanlder.handle_request(mapping_request)
+
+
 
 if __name__ == '__main__':
-    main()
+    try:
+        handler = ServerHandler()
+    except rospy.ROSInterruptException:
+                pass
---
+++ ros_ws/src/pinit_pkg/src/server_communication/server_mapping_handler.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+from enum import Enum
+from proto.ros import ros_pb2_grpc
+from proto.ros import ros_pb2
+
+from ros_ws.src.pinit_pkg.src.robot_motion.motion_controller import MotionController
+
+import roslaunch
+import rospy
+
+
+
+class ServerMappingHandler():
+
+    class RobotState(Enum):
+        IDLE = 1
+        MAPPING = 2
+
+
+    def __init__(self):
+
+        self.motion_controller = MotionController()
+        self.current_state = self.RobotState.IDLE
+        self.launch_file_path = ""
+        self.launch = None
+        self.transitions = None
+
+        self.init_valid_transitions()
+
+
+    def init_valid_transitions(self):
+        """initializes a dictionary of state transitions"""
+        self.transitions = {
+            self.RobotState.MAPPING :
+            [self.RobotState.IDLE,
+             self.RobotState.MAPPING],
+            self.RobotState.IDLE:
+            [self.RobotState.MAPPING,
+             self.RobotState.IDLE]
+        }
+
+
+    def handle_request(self, request):
+        """handle grpcs mapping_request"""
+        request_type = request.request_type
+        direction = request.direction
+        if request_type == ros_pb2.ServerToRosMappingRequest.START_MAPPING:
+            self.start_mapping()
+        elif request_type == ros_pb2.ServerToRosMappingRequest.STOP_MAPPING:
+            self.stop_mapping()
+        else:
+            if direction == ros_pb2.ServerToRosMappingRequest.FORWARD:
+                self.move(MotionController.RobotDirection.FORWARD)
+            elif direction == ros_pb2.ServerToRosMappingRequest.BACKWARD:
+                self.move(MotionController.RobotDirection.BACKWARD)
+            elif direction == ros_pb2.ServerToRosMappingRequest.LEFT:
+                self.move(MotionController.RobotDirection.LEFT)
+            elif direction == ros_pb2.ServerToRosMappingRequest.RIGHT:
+                self.move(MotionController.RobotDirection.RIGHT)
+            else:
+                self.move(MotionController.RobotDirection.STOP)
+
+
+    def goto_state(self, state):
+        """transistion to another state if valid"""
+        valid_transitions = self.transitions[self.current_state]
+
+        if state in valid_transitions:
+            self.current_state = state
+        else:
+            print("Warning invalid state transistion")
+
+
+    def start_mapping(self):
+        """starts ros mapping launch file"""
+        self.goto_state(self.RobotState.MAPPING)
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(uuid)
+        self.launch = roslaunch.parent.ROSLaunchParent(uuid,\
+                    [self.launch_file_path])
+
+
+    def stop_mapping(self):
+        """kills ros mapping launch file"""
+        self.goto_state(self.RobotState.IDLE)
+
+        self.launch.shutdown()
+
+
+    def move(self, direction):
+        """moves the robot by publishing to /cmd_vel"""
+        self.goto_state(self.RobotState.MAPPING)
+        self.motion_controller.move(direction)
