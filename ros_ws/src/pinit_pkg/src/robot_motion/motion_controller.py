fix rotation sudden stop and other unecessary code removals

ros_ws/src/pinit_pkg/src/robot_motion/motion_controller.py
@@ -33,7 +33,7 @@ class MotionController():
         self.direction_lock = None
         self.vel_linear_range = (-0.5, 0.5)
         self.vel_angular_range = (-2.25, 2.25)
-        self.acceleration_steps = 200
+        self.acceleration_steps = 100
         self.vel_linear_increment = (abs(self.vel_linear_range[0] -
                                         self.vel_linear_range[1]) /
                                      self.acceleration_steps)
@@ -93,11 +93,11 @@ class MotionController():
                 else:
                     linear = linear
                 if angular > 0:
-                    angular = linear - self.vel_angular_increment
-                    angular = max(linear, 0)
+                    angular = angular - self.vel_angular_increment
+                    angular = max(angular, 0)
                 elif angular < 0:
-                    angular = linear + self.vel_angular_increment
-                    angular = min(linear, 0)
+                    angular = angular + self.vel_angular_increment
+                    angular = min(angular, 0)
                 else:
                     angular = angular
 
@@ -108,13 +108,6 @@ class MotionController():
             self.vel_linear = linear
             self.vel_angular = angular
             self.publish()
-        print "wtf"
-
-
-    def to_range(self, start, end):
-        """returns a fixed list with length=self.acceleration_steps elements between start and end"""
-
-        return np.linspace(start, end, self.acceleration_steps)
 
 
     def publish(self):