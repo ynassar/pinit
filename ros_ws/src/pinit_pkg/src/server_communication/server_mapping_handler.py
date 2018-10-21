df61ac325d91f4249a22119cba24bba0223528d8
Alfred <kareemfarid@aucegypt.edu>
2018-10-21 20:22:30 +0200

    added proper states now + small bug fix

--- ros_ws/src/pinit_pkg/src/server_communication/server_mapping_handler.py
+++ ros_ws/src/pinit_pkg/src/server_communication/server_mapping_handler.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 from enum import Enum
 from proto.ros import ros_pb2_grpc
 from proto.ros import ros_pb2
@@ -6,6 +7,8 @@ from proto.ros import ros_pb2
 from ros_ws.src.pinit_pkg.src.robot_motion.motion_controller import MotionController
 
 import roslaunch
+
+
 import rospy
 import rospkg
 
@@ -35,15 +38,14 @@ class ServerMappingHandler():
         """initializes a dictionary of state transitions"""
         self.transitions = {
             self.RobotState.MAPPING :
-            [self.RobotState.IDLE,
-             self.RobotState.MOVING_AND_MAPPING],
+                                    [self.RobotState.IDLE,
+                                    self.RobotState.MOVING_AND_MAPPING],
             self.RobotState.IDLE:
-            [self.RobotState.MAPPING,
-             self.RobotState.MOVING_AND_MAPPING,
-             self.RobotState.IDLE],
+                                    [self.RobotState.MAPPING,
+                                    self.RobotState.IDLE],
             self.RobotState.MOVING_AND_MAPPING :
-            [self.RobotState.IDLE,
-             self.RobotState.MOVING_AND_MAPPING]
+                                    [self.RobotState.IDLE,
+                                    self.RobotState.MOVING_AND_MAPPING]
         }
 
 
@@ -71,30 +73,41 @@ class ServerMappingHandler():
     def goto_state(self, state):
         """transistion to another state if valid"""
         valid_transitions = self.transitions[self.current_state]
+        success = False
 
         if state in valid_transitions:
             self.current_state = state
+            success = True
         else:
             print("Warning invalid state transistion")
+            success = False
+
+        return success
 
 
     def start_mapping(self):
         """starts ros mapping launch file"""
-        self.goto_state(self.RobotState.MAPPING)
-        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        roslaunch.configure_logging(uuid)
-        self.launch = roslaunch.parent.ROSLaunchParent(uuid,\
-                    [self.launch_file_path])
+        if (self.goto_state(self.RobotState.MAPPING)):
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            self.launch = roslaunch.parent.ROSLaunchParent(uuid,\
+            [self.launch_file_path])
+
+            self.launch.start()
 
 
     def stop_mapping(self):
         """kills ros mapping launch file"""
-        self.goto_state(self.RobotState.IDLE)
-
-        self.launch.shutdown()
+        if self.goto_state(self.RobotState.IDLE):
+            self.launch.shutdown()
 
 
     def move(self, direction):
         """moves the robot by publishing to /cmd_vel"""
-        self.goto_state(self.RobotState.MOVING_AND_MAPPING)
-        self.motion_controller.move(direction)
+        if self.goto_state(self.RobotState.MOVING_AND_MAPPING):
+            self.motion_controller.move(direction)
+
+
+if __name__ == '__main__':
+    t = ServerMappingHandler()
+    t.start_mapping()
