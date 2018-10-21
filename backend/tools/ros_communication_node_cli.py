2333b75d324eb9adc23710be6a65e11d03da3835
Yousef Nassar <ynassar@github.com>
2018-10-21 15:03:28 +0100

    Adds a handler for map communications from ROS, and extends the CLI to simulate this.

--- backend/tools/ros_communication_node_cli.py
+++ backend/tools/ros_communication_node_cli.py
@@ -1,16 +1,30 @@
 import grpc
 
+import time
+
 from absl import app
 from absl import flags
 
 from proto.ros import ros_pb2_grpc
 from proto.ros import ros_pb2
 
-flags.DEFINE_integer("port", 50051, "The port to connect to on localhost.")
+flags.DEFINE_integer("port", 50052, "The port to connect to on localhost.")
 FLAGS = flags.FLAGS
 
 def GenerateRequest():
     yield ros_pb2.RosToServerCommunication(robot_name='client_test')
+    time.sleep(1)
+    yield ros_pb2.RosToServerCommunication(raw_map=ros_pb2.RawMap(
+        resolution=1,
+        height=5,
+        width=5,
+        data=[0,0,0,0,0,
+              0,1,1,1,0,
+              0,1,0,1,0,
+              0,1,1,1,0,
+              0,0,0,0,0]
+    ))
+
 
 def main(argv):
     with grpc.insecure_channel(f'localhost:{FLAGS.port}') as channel:
