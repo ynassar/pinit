1565bd11f4b667911b8697325f43e23d08cf3147
kareefardi <kareefardi@users.noreply.github.com>
2018-10-21 20:13:48 +0200

    Feature/ros motion controller loop (#29)
    * change to producer consumer, NEEDS TESTING
    
    * changed controller logic and fixed threading
    
    * added more states
    
    * fix rotation sudden stop and other unecessary code removals
    
    * clean up
    
    * misc changes

--- ros_ws/src/pinit_pkg/src/robot_motion/motion_controller.py
+++ ros_ws/src/pinit_pkg/src/robot_motion/motion_controller.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python
 
-import rospy
-import numpy as np
+from concurrent import futures
 from enum import Enum
+import threading
+import time
+
+import numpy as np
+
+import rospy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
 class MotionController():
-
-    def __init__(self):
-
-        self.node_name = 'motion_controller'
-        self.topic_name = 'cmd_vel'
-        self.vel_linear = 0
-        self.vel_linear_max = 0.4
-        self.vel_linear_min = -0.5
-        self.vel_angular = 0
-        self.vel_angular_max = 2.25
-        self.vel_angular_min = -2.25
-        self.acceleration_steps = 1000
-        self.vel_pub = rospy.Publisher(self.topic_name,
-                                       Twist,
-                                       queue_size=10)
-
 
     class RobotDirection(Enum):
         FORWARD = 0
@@ -32,14 +21,87 @@ class MotionController():
         STOP = 4
 
 
+    def __init__(self):
+
+        self.node_name = 'motion_controller'
+        self.topic_name = 'cmd_vel'
+        self.vel_linear = 0
+        self.vel_angular = 0
+        self.robot_direction = self.RobotDirection.STOP
+        self.direction_lock = None
+        self.vel_linear_range = (-0.5, 0.5)
+        self.vel_angular_range = (-2.25, 2.25)
+        self.acceleration_steps = 50
+        self.vel_linear_increment = (abs(self.vel_linear_range[0] -
+                                        self.vel_linear_range[1]) /
+                                     self.acceleration_steps)
+        self.vel_angular_increment = (abs(self.vel_angular_range[0] -
+                                          self.vel_angular_range[1]) /
+                                      self.acceleration_steps)
+        self.thread_executor = None
+        self.vel_pub = rospy.Publisher(self.topic_name,
+                                       Twist,
+                                       queue_size=10)
+
+        self.init_publisher_loop()
+
+
     def init_node(self):
         rospy.init_node(self.node_name, anonymous=True)
 
 
-    def to_range(self, start, end, n):
-        """returns a fixed list with n elements between start and end"""
+    def init_publisher_loop(self):
+       self.thread_executor = threading.Thread(target=self.publisher_loop)
+       self.thread_executor.start()
+       self.direction_lock = threading.Lock()
+       rospy.loginfo("velocity publisher loop init")
 
-        return np.linspace(start, end, n)
+
+    def publisher_loop(self):
+        while not rospy.is_shutdown():
+            rospy.sleep(0.01)
+            linear = self.vel_linear
+            angular = self.vel_angular
+            self.direction_lock.acquire()
+            direction = self.robot_direction
+            self.direction_lock.release()
+            if(direction == self.RobotDirection.FORWARD):
+                linear = linear + self.vel_linear_increment
+                linear = min(linear, self.vel_linear_range[1])
+            elif(direction == self.RobotDirection.BACKWARD):
+                linear = linear - self.vel_linear_increment
+                linear = max(linear, self.vel_linear_range[0])
+            elif(direction == self.RobotDirection.LEFT):
+                angular = angular + self.vel_angular_increment
+                angular = min(angular, self.vel_angular_range[1])
+            elif(direction == self.RobotDirection.RIGHT):
+                angular = angular - self.vel_angular_increment
+                angular = max(angular, self.vel_angular_range[0])
+            elif(direction == self.RobotDirection.STOP):
+                if linear > 0:
+                    linear = linear - self.vel_linear_increment
+                    linear = max(linear, 0)
+                elif linear < 0:
+                    linear = linear + self.vel_linear_increment
+                    linear = min(linear, 0)
+                else:
+                    linear = linear
+                if angular > 0:
+                    angular = angular - self.vel_angular_increment
+                    angular = max(angular, 0)
+                elif angular < 0:
+                    angular = angular + self.vel_angular_increment
+                    angular = min(angular, 0)
+                else:
+                    angular = angular
+
+            else:
+                rospy.logwarn("Unknown motion direction. Check \
+                                 MotionController.RobotDirection")
+
+            self.vel_linear = linear
+            self.vel_angular = angular
+            self.publish()
 
 
     def publish(self):
@@ -51,40 +113,12 @@ class MotionController():
 
 
     def move(self, direction):
-        vel_linear, vel_angular = self.get_speeds(direction)
-        n = len(vel_linear)
-        for i in range(n):
-            self.vel_linear = vel_linear[i]
-            self.vel_angular = vel_angular[i]
-            self.publish()
-
-
-    def get_speeds(self, direction):
-        linear = []
-        angular = []
-        if(direction == self.RobotDirection.FORWARD):
-            linear = self.to_range(0, self.vel_linear_max, self.acceleration_steps)
-            angular = self.to_range(0, 0, self.acceleration_steps)
-        elif(direction == self.RobotDirection.BACKWARD):
-            linear = self.to_range(0, self.vel_linear_min, self.acceleration_steps)
-            angular = self.to_range(0, 0, self.acceleration_steps)
-        elif(direction == self.RobotDirection.RIGHT):
-            linear = self.to_range(0, 0, self.acceleration_steps)
-            angular = self.to_range(0, self.vel_angular_max, self.acceleration_steps)
-        elif(direction == self.RobotDirection.LEFT):
-            linear = self.to_range(0, 0, self.acceleration_steps)
-            angular = self.to_range(0, self.vel_angular_min, self.acceleration_steps)
-        elif(direction == self.RobotDirection.STOP):
-            linear = self.to_range(self.vel_linear, 0, self.acceleration_steps)
-            angular = self.to_range(self.vel_angular, 0, self.acceleration_steps)
-        else:
-            raise ValueError("Unknown motion direction. Check \
-                              MotionController.RobotDirection")
-
-        return linear, angular
+        self.direction_lock.acquire()
+        self.robot_direction = direction
+        self.direction_lock.release()
+        rospy.loginfo("direction set" + str(direction))
 
 
 
 if __name__ == '__main__':
     controller = MotionController()
-    controller.init_node()
