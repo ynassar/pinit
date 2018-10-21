e2203d5a567a1282e5cee38acb3fe05dbf8edf90
kareefardi <kareefardi@users.noreply.github.com>
2018-10-21 20:19:57 +0200

    added map streamer (#31)

--- ros_ws/src/pinit_pkg/src/server_communication/server_interface.py
+++ ros_ws/src/pinit_pkg/src/server_communication/server_interface.py
@@ -6,8 +6,11 @@ from proto.ros import ros_pb2_grpc
 from proto.ros import ros_pb2
 
 from enum import Enum
+from Queue import Queue
+import signal
 
 from server_mapping_handler import ServerMappingHandler
+from server_map_streamer import ServerMapStreamer
 
 import rospy
 
@@ -18,34 +21,49 @@ class ServerHandler():
 
         self.robot_name = "nemo"
         self.node_name = "robot_grpc_server_handler"
-        self.mapping_hanlder = ServerMappingHandler()
-    
         self.init_node()
+        self.communication_queue = Queue()
+
+        self.mapping_hanlder = ServerMappingHandler()
+        self.map_streamer = ServerMapStreamer(self.communication_queue)
+
         self.main_loop()
 
 
+
     def init_node(self):
-        rospy.init_node(self.node_name, anonymous=False)
+        rospy.init_node(self.node_name,
+                        anonymous=False,
+                        disable_signals=True)
 
 
     def stub_callback(self):
         yield ros_pb2.RosToServerCommunication(robot_name=self.robot_name)
+        for communication in iter(self.communication_queue.get, None):
+            yield communication
 
 
     def main_loop(self):
-        while not rospy.is_shutdown():
-            with grpc.insecure_channel('10.40.54.241:50052') as channel:
-                stub = ros_pb2_grpc.RosServiceStub(channel)
-                for communication in stub.Communicate(self.stub_callback()):
-                    print communication
-                    if communication.HasField("mapping_request"):
-                        mapping_request = communication.mapping_request
-                        self.mapping_hanlder.handle_request(mapping_request)
+        MAX_MESSAGE_LENGTH = 1024 * 1024 * 10
+        with grpc.insecure_channel('10.40.37.149:50052',
+                                   options=[
+                                       ("grpc.max_send_message_length", MAX_MESSAGE_LENGTH),
+                                       ("grpc.max_receive_message_length", MAX_MESSAGE_LENGTH)]) as channel:
+            stub = ros_pb2_grpc.RosServiceStub(channel)
+            for communication in stub.Communicate(self.stub_callback()):
+                print(communication)
+                if communication.HasField("mapping_request"):
+                    mapping_request = communication.mapping_request
+                    self.mapping_hanlder.handle_request(mapping_request)
+
 
 
 
 if __name__ == '__main__':
     try:
+        print("running as main")
         handler = ServerHandler()
-    except rospy.ROSInterruptException:
-                pass
+    except rospy.ROSInterruptException as e:
+        print("fasfa", e)
+        pass
+
