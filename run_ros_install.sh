763f718cb5518d21f4e5914e1d61110216debb4b
kareefardi <kareemfarid@aucegypt.edu>
2018-10-28 18:50:34 +0200

    some fixes and added some prints

--- run_ros_install.sh
+++ run_ros_install.sh
@@ -1,7 +1,8 @@
-echo "Installing ros base follwoing ros kinetic online guide..."
+echo "Installing ros base following ros kinetic online guide..."
+echo "Updating ubuntu source list.."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get -y -q update 
+sudo apt-get -y -qq update 
 
 ros_setup_packages=(
 	"ros-kinetic-ros-base"
@@ -11,14 +12,17 @@ ros_setup_packages=(
 	"build-essential"
 )
 
-sudo apt-get install -q -y ${ros_setup_packages[@]}
+echo "Installing ros base packages..."
+sudo apt-get install -qq -y ${ros_setup_packages[@]}
 
+source /opt/ros/kinetic/setup.bash
+
+echo "Other ros initilizations..."
 sudo rosdep init
 rosdep update
 
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 
-echo "Installing imu dependancies..."
 imu_packages=(
 	"libi2c-dev"
 	"libeigen3-dev"
@@ -26,7 +30,11 @@ imu_packages=(
 	"ros-kinetic-imu-tools"
 )
 
+
+echo "Installing imu dependancies..."
 sudo apt-get install -q -y ${imu_packages[@]}
 
-echo "Installing ros messages"
+echo "Installing ros messages..."
 sudo apt install -q -y ros-kinetic-common-msgs
+
+echo "Done!"
