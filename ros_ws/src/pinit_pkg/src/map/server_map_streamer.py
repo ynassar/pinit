0415116a543fe3c46e2ba9ed9a43ca0dc7c4f6c4
kareefardi <kareefardi@users.noreply.github.com>
2018-11-11 11:32:05 +0200

    Feature/ros gps to map (#57)
    * Added gps controller
    
    * Aded gps utils file
    
    * calculate the distance and bearing between two gps coordinates
    
    * minor refactor.. added a gps point struct
    
    * fix docs
    
    * add gps driver
    
    * change naming
    
    * change folder structure
    
    * fetch gps origin and send it with the map

--- ros_ws/src/pinit_pkg/src/map/server_map_streamer.py
+++ ros_ws/src/pinit_pkg/src/map/server_map_streamer.py
@@ -8,10 +8,10 @@ import rospy
 from nav_msgs.msg import MapMetaData
 from nav_msgs.msg import OccupancyGrid
 
+from gps.gps_controller import GpsController
+
 
 class ServerMapStreamer():
-    #TODO start and stop mapping streaming the map programatically
-
     """Fetch the map and send it to the server"""
 
     def __init__(self, queue):
@@ -19,10 +19,11 @@ class ServerMapStreamer():
         self.map_topic_name = "map"
         self.map_grpc = None
         self.subscriber = None
+        self.global_origin = None
 
 
     def start_stream(self):
-        """Start listenning and streaming the mapping
+        """Start listenning and streaming the mapping and gets global reference
 
         Args:
             None
@@ -31,9 +32,23 @@ class ServerMapStreamer():
             None
         """
 
+        self.fetch_global_origin() #This should block until we receive gps coordinates
         self.subscriber = rospy.Subscriber(self.map_topic_name,
                                            OccupancyGrid,
                                            self.map_callback)
+
+
+    def fetch_global_origin(self):
+        """Fetch the map global origin
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+
+        self.global_origin = GpsController().get_coordinates()
 
 
     def stop_stream(self):
@@ -48,8 +63,10 @@ class ServerMapStreamer():
 
         self.subscriber.unregister()
 
+
     def map_callback(self, occupancy_grid):
         """Queue the map in the server main queue
+
 
         Args:
             None
@@ -62,12 +79,17 @@ class ServerMapStreamer():
         map_raw_data = occupancy_grid.data
         map_raw_data_encoded = self.encode(map_raw_data)
 
+        gps_coordinates_msg = ros_pb2.GpsCoordinates(
+            longitude=self.global_origin.long,
+            latitude=self.global_origin.lat)
+
         self.map_grpc = ros_pb2.RosToServerCommunication(
             raw_map=ros_pb2.RawMap(
                 resolution=metadata.resolution,
                 height=metadata.height,
                 width=metadata.width,
-                data=map_raw_data_encoded))
+                data=map_raw_data_encoded,
+                coordinates=gps_coordinates_msg))
 
         self.communication_queue.put(self.map_grpc)
         rospy.loginfo("Sending map to server...")
@@ -84,3 +106,4 @@ class ServerMapStreamer():
         """
 
         return (np.array(list_of_ints) + 1).astype('uint8').tobytes()
+
