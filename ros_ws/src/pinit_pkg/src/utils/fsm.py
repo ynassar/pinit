02916b9d70aa49c04203441822851475f0e11543
kareefardi <kareemfarid@aucegypt.edu>
2018-11-14 14:55:58 +0200

    change transition function to accept agruments

--- ros_ws/src/pinit_pkg/src/utils/fsm.py
+++ ros_ws/src/pinit_pkg/src/utils/fsm.py
@@ -9,9 +9,9 @@ class FSM():
             self.call_back = call_back
 
 
-        def execute(self):
+        def execute(self, *args):
             try:
-                self.call_back()
+                self.call_back(*args)
             except Exception:
                 raise
 
@@ -44,18 +44,22 @@ class FSM():
             print "State {s} doesn't exist".format(s=state)
 
 
-    def go_to(self, target_state):
+    def go_to(self, target_state, *args):
+        success = False
         transtition = self.get_transition(self.current_state, target_state)
         if transtition is not None:
             try:
-                transtition.execute()
+                transtition.execute(*args)
                 self.current_state = target_state
+                success = True
             except Exception:
                 raise
         else:
             print "Invalid transtion from {source} to {target}".format(
                 source=self.current_state,
                 target=target_state)
+
+        return success
 
 
     def get_transition(self, source, target):
@@ -81,8 +85,8 @@ if __name__ == '__main__':
     state2 = 's2'
     test_fsm = FSM()
 
-    def cb1():
-        print "cb1"
+    def cb1(lol):
+        print lol
     def cb2():
         print "cb2"
 
@@ -90,5 +94,5 @@ if __name__ == '__main__':
     test_fsm.add_state(state2)
     test_fsm.set_default(state1)
     test_fsm.add_transition(state1, state2, cb1)
-    test_fsm.go_to(state2)
+    test_fsm.go_to(state2, "hello")
     test_fsm.go_to(state1)
