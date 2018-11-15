c204628d74b6580f923d937fb13e7266ddd30a15
kareefardi <kareemfarid@aucegypt.edu>
2018-11-15 13:15:59 +0200

    not a static method anymore and filled transitions callbacks

--- ros_ws/src/pinit_pkg/src/robot_state/state_manager.py
+++ ros_ws/src/pinit_pkg/src/robot_state/state_manager.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 from enum import Enum
-from utils.fsm import FSM
+from Queue import Queue
 import rospkg
 import roslaunch
 import rospy
+
+from utils.fsm import FSM
+from nodes.node_manager import NodeManager
+from map.map_creator import MapCreator
+from map.map_streamer import MapStreamer
+from robot_motion.motion_controller import MotionController
+from server_communication.server_pose_streamer import ServerPoseStreamerFactory
 
 
 class RobotStateManager():
@@ -15,100 +22,120 @@ class RobotStateManager():
         NAVIGATING = 4
         ERROR = 5
 
-    # static vars
-    robot_fsm = FSM()
-    states = [s for s in States]
-
-
-    def __init__(self):
-        raise NotImplementedError('This class is intended to be used as a static class only')
-
 
     @classmethod
-    def go_to(cls, state):
-        cls.robot_fsm.go_to(state)
+    def create(cls):
+        robot_fsm = FSM()
+        communication_queue = Queue()
+        node_manager = NodeManager()
+        map_creator = MapCreator()
+        map_streamer = MapStreamer(communication_queue)
+        pose_streamer = ServerPoseStreamerFactory(communication_queue)
+        motion_controller = MotionController()
+
+        return RobotStateManager(
+            robot_fsm=robot_fsm,
+            com_queue=communication_queue,
+            node_manager=node_manager,
+            map_streamer=map_streamer,
+            pose_streamer=pose_streamer,
+            motion_controller=motion_controller)
 
 
-    @classmethod
-    def init_states(cls):
-        for state in cls.states:
-            cls.robot_fsm.add_state(state)
-        cls.robot_fsm.set_default(cls.States.START)
+    def __init__(self, robot_fsm, com_queue, node_manager, map_streamer, pose_streamer,
+                 motion_controller):
+
+        self.fsm_states = [s for s in self.States]
+        self.robot_fsm = robot_fsm
+        self.communication_queue = com_queue
+        self.node_manager = node_manager
+        self.map_streamer = map_streamer
+        self.pose_streamer = pose_streamer
+        self.motion_controller = motion_controller
+
+        self.init_states()
+        self.init_transitions()
 
 
-    @classmethod
-    def init_transitions(cls):
-        cls.robot_fsm.add_transition(cls.States.START,
-                                      cls.States.IDLE,
-                                      cls.start_to_idle_cb)
-        cls.robot_fsm.add_transition(cls.States.IDLE,
-                                      cls.States.IDLE,
-                                      cls.idle_to_idle_cb)
-        cls.robot_fsm.add_transition(cls.States.IDLE,
-                                      cls.States.MAPPING,
-                                      cls.idle_to_mapping_cb)
-        cls.robot_fsm.add_transition(cls.States.IDLE,
-                                      cls.States.NAVIGATING,
-                                      cls.idle_to_navigating_cb)
-        cls.robot_fsm.add_transition(cls.States.MAPPING,
-                                      cls.States.MAPPING,
-                                      cls.mapping_to_mapping_cb)
-        cls.robot_fsm.add_transition(cls.States.MAPPING,
-                                      cls.States.IDLE,
-                                      cls.mapping_to_idle_cb)
-        cls.robot_fsm.add_transition(cls.States.NAVIGATING,
-                                      cls.States.NAVIGATING,
-                                      cls.navigating_to_navigating_cb)
-        cls.robot_fsm.add_transition(cls.States.NAVIGATING,
-                                      cls.States.IDLE,
-                                      cls.navigating_to_idle_cb)
+    def go_to(self, state, *args):
+        self.robot_fsm.go_to(state, *args)
 
 
-    @classmethod
-    def idle_to_idle_cb(cls):
-        pass
-
-    
-    @classmethod
-    def idle_to_mapping_cb(cls):
-        pass
+    def init_states(self):
+        for state in self.fsm_states:
+            self.robot_fsm.add_state(state)
+        self.robot_fsm.set_default(self.States.START)
 
 
-    @classmethod
-    def idle_to_navigating_cb(cls):
-        pass
+    def init_transitions(self):
+        self.robot_fsm.add_transition(self.States.START,
+                                      self.States.IDLE,
+                                      self.start_to_idle_cb)
+        self.robot_fsm.add_transition(self.States.IDLE,
+                                      self.States.IDLE,
+                                      self.idle_to_idle_cb)
+        self.robot_fsm.add_transition(self.States.IDLE,
+                                      self.States.MAPPING,
+                                      self.idle_to_mapping_cb)
+        self.robot_fsm.add_transition(self.States.IDLE,
+                                      self.States.NAVIGATING,
+                                      self.idle_to_navigating_cb)
+        self.robot_fsm.add_transition(self.States.MAPPING,
+                                      self.States.MAPPING,
+                                      self.mapping_to_mapping_cb)
+        self.robot_fsm.add_transition(self.States.MAPPING,
+                                      self.States.IDLE,
+                                      self.mapping_to_idle_cb)
+        self.robot_fsm.add_transition(self.States.NAVIGATING,
+                                      self.States.NAVIGATING,
+                                      self.navigating_to_navigating_cb)
+        self.robot_fsm.add_transition(self.States.NAVIGATING,
+                                      self.States.IDLE,
+                                      self.navigating_to_idle_cb)
 
 
-    @classmethod
-    def mapping_to_idle_cb(cls):
-        pass
-
-
-    @classmethod
-    def mapping_to_mapping_cb(cls):
-        pass
-
-
-    @classmethod
-    def navigating_to_idle_cb(cls):
-        pass
-
-    
-    @classmethod
-    def navigating_to_navigating_cb(cls):
+    def idle_to_idle_cb(self):
         pass
 
 
-    @classmethod
-    def start_to_idle_cb(cls):
-        pass       
+    def idle_to_mapping_cb(self):
+        self.node_manager.start_gmapping()
+        self.map_streamer.start()
+
+
+    def idle_to_navigating_cb(self):
+        pass
+
+
+    def mapping_to_idle_cb(self):
+        self.map_streamer.finish()
+        self.node_manager.stop_gmapping()
+        self.node_manager.start_movebase()
+
+
+    def mapping_to_mapping_cb(self, *args):
+        self.motion_controller.move(*args)
+
+
+    def navigating_to_idle_cb(self):
+        pass
+
+
+    def navigating_to_navigating_cb(self):
+        pass
+
+
+    def start_to_idle_cb(self):
+        self.node_manager.start_robot()
+        self.pose_streamer.init_stream_loop()
 
 
 if __name__ == "__main__":
     rospy.init_node("robot_state_manager_test")
-    RobotStateManager.init_states()
-    RobotStateManager.init_transitions()
-    RobotStateManager.go_to(RobotStateManager.States.IDLE)
+    manager = RobotStateManager.create()
+    manager.init_states()
+    manager.init_transitions()
+    manager.go_to(RobotStateManager.States.IDLE)
     rospy.sleep(10)
 
 
