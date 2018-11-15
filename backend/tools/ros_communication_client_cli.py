98531b5076015cedd7b9156f9336f712e74c2de6
kareefardi <kareemfarid@aucegypt.edu>
2018-11-15 13:18:17 +0200

    add mapping with moving scenario

--- backend/tools/ros_communication_client_cli.py
+++ backend/tools/ros_communication_client_cli.py
@@ -17,7 +17,7 @@ def main(argv):
     with grpc.insecure_channel(f'localhost:{FLAGS.port}') as channel:
         with grpc.insecure_channel(f'localhost:50051') as auth_channel:
             auth_stub = login_pb2_grpc.AuthenticationServiceStub(auth_channel)
-            response = auth_stub.Login(login_pb2.LoginRequest(username="y", password="p"))
+            response = auth_stub.Login(login_pb2.LoginRequest(username="Admin", password="123"))
             token = response.token
         stub = ros_pb2_grpc.RosServiceStub(channel)
         stub.SendMovement(
@@ -26,6 +26,22 @@ def main(argv):
                 mapping_request=ros_pb2.ServerToRosMappingRequest(
                     request_type=ros_pb2.ServerToRosMappingRequest.START_MAPPING
                 )))
+        time.sleep(3)
+        stub.SendMovement(
+            ros_pb2.MappingRequest(
+                token=token,
+                mapping_request=ros_pb2.ServerToRosMappingRequest(
+                    request_type=ros_pb2.ServerToRosMappingRequest.DIRECTION,
+                    direction=ros_pb2.ServerToRosMappingRequest.FORWARD
+                    )))
+        time.sleep(3)
+        stub.SendMovement(
+            ros_pb2.MappingRequest(
+                token=token,
+                mapping_request=ros_pb2.ServerToRosMappingRequest(
+                    request_type=ros_pb2.ServerToRosMappingRequest.DIRECTION,
+                    direction=ros_pb2.ServerToRosMappingRequest.STOP
+                    )))
         time.sleep(3)
         stub.SendMovement(
             ros_pb2.MappingRequest(
