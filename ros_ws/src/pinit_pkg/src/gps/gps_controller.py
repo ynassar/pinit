bc16ca51fae8f48401b810f7133de6288701af03
kareefardi <kareemfarid@aucegypt.edu>
2018-11-18 14:50:07 +0200

    changed to a gps_fix msg subscriber

--- ros_ws/src/pinit_pkg/src/gps/gps_controller.py
+++ ros_ws/src/pinit_pkg/src/gps/gps_controller.py
@@ -19,9 +19,7 @@ class GpsController():
     
     def __init__(self):
         self.node_name = "gps_controller"
-        self.launch_file_path = rospkg.RosPack().get_path('pinit') + \
-            "/launch/gps_launch.launch"
-
+        self.subscriber = rospy.Subscriber("fix", NavSatFix, self.callback)
         self.longitude = None
         self.latitude = None
         
@@ -35,32 +33,17 @@ class GpsController():
 
 
     def get_coordinates(self):
-        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        roslaunch.configure_logging(uuid)
-        self.launch = roslaunch.parent.ROSLaunchParent(uuid,\
-            [self.launch_file_path])
-        self.launch.start()
-
-
-        sub = rospy.Subscriber("fix", NavSatFix, self.callback)
-
-        while(self.longitude == None or self.latitude == None \
-            or math.isnan(self.longitude)):
-            # rospy.loginfo("Waiting for coordinates")
-            pass
 
         longitude = self.longitude
         latitude = self.latitude
-        print self.latitude
-        sub.unregister()
-        self.launch.shutdown()
-        self.longitude = None
-        self.latitude = None
 
-        point = GpsPoint()
-        point.lat = latitude
-        point.long = longitude
+        point = GpsPoint(
+            long=longitude, 
+            lat=latitude)
+        
         return point
+
+
 
 if __name__ == '__main__':
     try:
