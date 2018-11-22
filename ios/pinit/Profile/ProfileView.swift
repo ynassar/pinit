df7b8c4f7a9c57130d97fe16c7158b5093b88910
Farida Eid <farida.eid@aucegypt.edu>
2018-11-22 13:18:15 +0200

    Ios feature/refactor ui (#78)
    * move mapping folder from the project to pinit owner
    
    * Start refactoring the pinit user homepage
    
    * move mapping to pinit owner folder
    
    * Add robot request server in robot request view controller
    
    * fixed the issue where geting the gps location is called more than once
    
    * Adding the side profile menu and making sure the transitions are working
    
    * fixing the bug with the fixed frame
    
    * fix the logout button bug
    
    * add Icons and create a homepage for the admin
    
    * change the icons and make some changes in the profile
    
    * changes in the profile side menu
    
    * final changes in the UI
    
    * ++

--- ios/pinit/Profile/ProfileView.swift
+++ ios/pinit/Profile/ProfileView.swift
@@ -4,15 +4,29 @@ class ProfileView : UIView, PinitViewProtocol {
     
     public var logoutButton: UIButton
     
+    public var closeButton: UIButton!
+    
+    public var usernameLabel: UILabel!
+    
     init() {
         logoutButton = UIButton(frame: CGRect.zero)
+        closeButton = UIButton(frame: CGRect.zero)
+        usernameLabel = UILabel(frame: CGRect.zero)
         super.init(frame: CGRect.zero)
         self.addSubview(logoutButton)
+        self.addSubview(usernameLabel)
+        self.addSubview(closeButton)
     }
     
     public func updateView() {
         logoutButton.setTitle("Logout", for: .normal)
+        logoutButton.backgroundColor = .white
+        logoutButton.setTitleColor(.black, for: .normal)
         
+        closeButton.setImage(UIImage(named: "closeIconWhite"), for: .normal)
+        
+        let spacing = self.frame.size.width * 0.05
+
         logoutButton = logoutButton
             .addCenterXConstraint(relativeView: self)
             .addCenterYConstraint(relativeView: self)
@@ -22,6 +36,31 @@ class ProfileView : UIView, PinitViewProtocol {
         logoutButton.layer.cornerRadius = 10
         logoutButton.titleLabel?.font = logoutButton.titleLabel?.font
             .withSize(PinitConstants.authenticationFontSize)
+
+        closeButton = closeButton
+            .addWidthConstraint(relativeView: self, multipler: 0.1)
+            .setEqualConstraint(selfAttribute: .height,
+                                relativeView: closeButton,
+                                relativeAttribute: .width)
+            .setConstraintWithConstant(selfAttribute: .top,
+                                       relativeView: self,
+                                       relativeAttribute: .top,
+                                       constant: UIApplication.shared.statusBarFrame.size.height)
+            .setConstraintWithConstant(selfAttribute: .right,
+                                       relativeView: self,
+                                       relativeAttribute: .right,
+                                       constant: -spacing)
+
+    }
+    
+    public func addGradiant() {
+        let gradiantLayer = CAGradientLayer()
+        gradiantLayer.frame = self.bounds
+        gradiantLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradiantLayer.endPoint = CGPoint(x: 1, y: 0)
+        gradiantLayer.colors = [PinitColors.yellow.cgColor,
+                                PinitColors.red.cgColor]
+        self.layer.insertSublayer(gradiantLayer, at: 0)
 
     }
     
