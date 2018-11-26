a51bc7f649957025e595a2e1f514815454e164fc
Omar Nawawy <omarnawawy@Omars-MacBook-Pro.local>
2018-11-26 18:28:39 +0200

    Modified initial pose publisher

--- ros_ws/src/pinit_pkg/src/robot_navigation/initial_pose_publisher.py
+++ ros_ws/src/pinit_pkg/src/robot_navigation/initial_pose_publisher.py
@@ -7,29 +7,22 @@ from std_msgs.msg import Int32
 
 class InitialPosePublisher(object):
     def __init__(self):
-        publisher = rospy.Publisher('/initialpose', PoseWithCovarianceStamped)
-
-        # rate = rospy.Rate(10)
-
-        # while not rospy.is_shutdown():
-
-        initial_pose = PoseWithCovarianceStamped()
-        initial_pose.header.frame_id = "map"
-        initial_pose.header.stamp = rospy.Time.now()
-
-        #Position(5.107, 4.244, 0.000), Orientation(0.000, 0.000, 0.936, -0.353)
-        point = Point(5.107, 4244, 0.000)
-        orientation = Quaternion(0.000, 0.000, 0.0936, -0.0353)
-
-
-
-        initial_pose.pose.pose.position = point
-        initial_pose.pose.pose.orientation = orientation
-
+        self.publisher = rospy.Publisher('/initialpose', PoseWithCovarianceStamped) 
+        self.initial_pose = PoseWithCovarianceStamped()
+        self.initial_pose.header.frame_id = "map"
+        self.initial_pose.header.stamp = rospy.Time.now()
         rospy.sleep(15)
-        print "published the pose"
-        publisher.publish(initial_pose)
+
+    def send_initial_pose(self, point, quaternion):        
+        self.initial_pose.pose.pose.position = point
+        self.initial_pose.pose.pose.orientation = orientation
+        self.publisher.publish(self.initial_pose)
 
 
 if __name__ == '__main__':
     initial_pose_publisher = InitialPosePublisher()
+    
+    point = Point(5.107, 4244, 0.000)
+    orientation = Quaternion(0.000, 0.000, 0.0936, -0.0353)
+
+    initial_pose_publisher.send_initial_pose(point, quaternion)
